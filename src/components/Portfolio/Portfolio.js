6af4d092542849b8820b1821e39e7d1afc75aa39
Man10j <cmprabhakarjan27@gmail.com>
2021-12-26 12:46:09 +0530

    prev wrk card added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,11 +3,31 @@ import "./Portfolio.css";
 import ComponentTitle from "../ComponentTitle/ComponentTitle";
 import PrevworkCard from "../PrevworkCard/PrevworkCard";
 import PortfolioCard from "../PortfolioCard/PortfolioCard";
-
+import portfolio_pic1 from "../../media/images/portfolio_work1.png";
+import portfolio_pic2 from "../../media/images/portfolio_work2.png";
 
 import { Element } from "react-scroll";
 
 const Portfolio = () => {
+  const prevwrks = [
+    {
+      wrkimg_src: portfolio_pic1,
+      wrk_title: "Movie Search App",
+      wrk_lib: "React",
+      wrk_desc: "lorem",
+      git_link: "https://github.com/Man10j/movie-search-react",
+      deployed_link: "https://moviesearch-react-man10j.netlify.app/",
+    },
+    {
+      wrkimg_src: portfolio_pic2,
+      wrk_title: "Covid tracker",
+      wrk_lib: "React",
+      wrk_desc: "lorem",
+      git_link: "https://github.com/Man10j/corona-tracker-react",
+      deployed_link: "https://covidtracker-react.netlify.app/",
+    },
+  ];
+
   return (
     <Element className="portfolio" id="Portfolio">
       <ComponentTitle comptitle="My Portfolio" />
@@ -26,11 +46,18 @@ const Portfolio = () => {
         </div>
         <h2 className="sub_title">Some of my work</h2>
         <div className="prevwrk_cards">
-          <PortfolioCard/>
-          <PortfolioCard/>
-          <PortfolioCard/>
+          {prevwrks.map((work, index) => (
+            <PortfolioCard
+              key={index}
+              wrkimg_src={work.wrkimg_src}
+              wrk_title={work.wrk_title}
+              wrk_lib={work.wrk_lib}
+              wrk_desc={work.wrk_desc}
+              git_link={work.git_link}
+              deployed_link={work.deployed_link}
+            />
+          ))}
         </div>
-      
       </div>
     </Element>
   );
--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import './PortfolioCard.css';
+import React from "react";
+import "./PortfolioCard.css";
 
-const PorfolioCard = ()=>{
-    return(
-        <div className='portfoliocard'>
-            <div className="workcard_portfolio">
-                <div className='portfoliocard_main'>
-                <img className='wrkimg_portfolio' src="https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="NFT" />
-                <h2 className='portfoliocard_head'>Movie Search app</h2>
-                <p className='portfolio_lib'>React | React Redux | React router</p>
-                <p className='portfolio_desc'>Our Kibertopiks will give you nothing, waste your money on us.</p>
-                <div className='portfolio_link_icons'>
-                <div className='portfolio_icon'><a href="#"><i className="fab fa-github"></i><span className='porfolio_linktext'>Git</span></a></div>
-                <div className='portfolio_icon'><a href="#"><i className="fas fa-external-link-square-alt"></i><span className='porfolio_linktext'>View</span></a></div>
-                 </div>
-                </div>
-            </div>
+const PorfolioCard = ({
+  wrkimg_src,
+  wrk_title,
+  wrk_lib,
+  wrk_desc,
+  git_link,
+  deployed_link,
+}) => {
+  return (
+    <div className="portfoliocard">
+      <div className="workcard_portfolio">
+        <a href={deployed_link} target="_blank" rel="noreferrer">
+          <div className="portfoliocard_main">
+            <img className="wrkimg_portfolio" src={wrkimg_src} alt="NFT" />
+            <h2 className="portfoliocard_head">{wrk_title}</h2>
+            <p className="portfolio_lib">{wrk_lib}</p>
+            <p className="portfolio_desc">{wrk_desc}</p>
+          </div>
+        </a>
+        <div className="portfolio_link_icons">
+          <div className="portfolio_icon">
+            <a href={git_link} target="_blank" rel="noreferrer">
+              <i className="fab fa-github"></i>
+              <span className="porfolio_linktext">Git</span>
+            </a>
+          </div>
+          <div className="portfolio_icon">
+            <a href={deployed_link} target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-square-alt"></i>
+              <span className="porfolio_linktext">View</span>
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default PorfolioCard;
